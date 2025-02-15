94c7f017889ef0a5d72b2088318a0508e8c61dcc
Jack <78124447+JackDerksen@users.noreply.github.com>
2025-02-14 21:00:41 -0700

    Delete nvim/lua/plugins/automaton.lua

--- nvim/lua/plugins/automaton.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "eandrju/cellular-automaton.nvim",
-  keys = {
-    { "<leader>ar", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
-    { "<leader>as", "<cmd>CellularAutomaton scramble<CR>", desc = "Scramble" },
-    { "<leader>ag", "<cmd>CellularAutomaton game_of_life<CR>", desc = "Game of Life" },
-  },
-}
