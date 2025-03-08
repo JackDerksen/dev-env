b573f717ff312119c3d31e8058417caba4ecb4d7
Jack <78124447+JackDerksen@users.noreply.github.com>
2025-03-07 21:04:26 -0700

    Add files via upload

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,68 @@
+return {
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		config = function()
+			modes = { insert = true, command = true, terminal = false }
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=]
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" }
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true
+			-- better deal with markdown code blocks
+			markdown = true
+		end,
+	},
+	{
+		"echasnovski/mini.clue",
+		version = false,
+		config = function()
+			local miniclue = require("mini.clue")
+			miniclue.setup({
+				triggers = {
+					-- Leader triggers
+					{ mode = "n", keys = "<Leader>" },
+					{ mode = "x", keys = "<Leader>" },
+
+					-- Built-in completion
+					{ mode = "i", keys = "<C-x>" },
+
+					-- `g` key
+					{ mode = "n", keys = "g" },
+					{ mode = "x", keys = "g" },
+
+					-- Marks
+					{ mode = "n", keys = "'" },
+					{ mode = "n", keys = "`" },
+					{ mode = "x", keys = "'" },
+					{ mode = "x", keys = "`" },
+
+					-- Registers
+					{ mode = "n", keys = '"' },
+					{ mode = "x", keys = '"' },
+					{ mode = "i", keys = "<C-r>" },
+					{ mode = "c", keys = "<C-r>" },
+
+					-- Window commands
+					{ mode = "n", keys = "<C-w>" },
+
+					-- `z` key
+					{ mode = "n", keys = "z" },
+					{ mode = "x", keys = "z" },
+				},
+
+				clues = {
+					-- Enhance this by adding descriptions for <Leader> mapping groups
+					miniclue.gen_clues.builtin_completion(),
+					miniclue.gen_clues.g(),
+					miniclue.gen_clues.marks(),
+					miniclue.gen_clues.registers(),
+					miniclue.gen_clues.windows(),
+					miniclue.gen_clues.z(),
+				},
+			})
+		end,
+	},
+}
